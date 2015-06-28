538fe4cab665c7a512e065e53c76b8aead9548bf
Tomáš Taro <tomas@getadblock.com>
2015-06-28 21:03:45 +0200

    Make install button working

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ $(document).ready(function () {
 	"use strict";
 	
 	// Let's detect the browser, and show a Safari downloads link if it's unsupported
-	
-	var UA = navigator.userAgent;
+	var chrome = window.chrome;
+    var opera = window.opr;
+    var safari = window.safari;
 	var $DLbutton = $("#downloadbtn");
 
-	if (UA.indexOf("OPR") > -1) {
+	if (opera) {
 		$DLbutton.text("Download for Opera");
 		$DLbutton.attr("href", "/opera");
-	}
-	else if (UA.indexOf("Chrome") > -1) {
+	} else if (chrome) {
 		$DLbutton.text("Download for Chrome");
 		$DLbutton.attr("href", "/chrome");
-	}
-	else {
+	} else if (safari) {
 		$DLbutton.text("Download for Safari");
 		$DLbutton.attr("href", "/safari");
-	}
+	} else {
+        $DLbutton.text("Unfortunately, CatBlock is not available for your browser.");
+        $DLbutton.attr("href", "#");
+    }
 });
