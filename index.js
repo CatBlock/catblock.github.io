81f5c80207c2fc3e2649082d325b119d6374d82b
Tomáš Taro <tomas@getadblock.com>
2017-03-25 15:17:27 +0100

    Update Safari download link
    Signed-off-by: Tomáš Taro <tomas@getadblock.com>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var safariURL = "//github.com/CatBlock/catblock/releases/download/v1.4.9/catblock-safari.safariextz";
+var safariURL = "//github.com/CatBlock/catblock/releases/download/v1.5.1/catblock-safari.safariextz";
 var firefoxURL = "//addons.mozilla.org/firefox/addon/adblock-with-catblock/";
 
 $(document).ready(function() {
